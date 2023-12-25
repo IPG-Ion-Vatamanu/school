3c50bcd7740f770aca36795fbb7ed87e1894b5bf
IPG-Ion-Vatamanu <154827361+IPG-Ion-Vatamanu@users.noreply.github.com>
2023-12-25 18:59:34 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+const now = new Date();
+
+const currentDateTime = now.toLocaleString();
+
+console.log(currentDateTime);
+
+const now = new Date();
+
+const currentDateTime = now.toLocaleString();
+
+console.log(currentDateTime);
